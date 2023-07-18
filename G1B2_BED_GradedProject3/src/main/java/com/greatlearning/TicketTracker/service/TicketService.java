922c36feef53c73e654ded700b68bd3e7e168147
NehaRajbhoj <123299752+NehaRajbhoj@users.noreply.github.com>
2023-07-18 16:17:41 +0530

    Add files via upload

---
+++ G1B2_BED_GradedProject3/src/main/java/com/greatlearning/TicketTracker/service/TicketService.java
@@ -0,0 +1,19 @@
+package com.greatlearning.TicketTracker.service;
+
+import java.util.List;
+
+import com.greatlearning.TicketTracker.model.Ticket;
+
+public interface TicketService {
+
+	Ticket addTicket(Ticket ticket);
+
+	Ticket getTicket(long ticketId);
+
+	List<Ticket> getAllTickets();
+
+	void deleteTicket(long ticketId);
+
+	Ticket editTicket(long ticketId, Ticket ticket);
+	List<Ticket> getByKeyword(String keyword);
+}
---
+++ G1B2_BED_GradedProject3/src/main/java/com/greatlearning/TicketTracker/service/TicketServiceImpl.java
@@ -0,0 +1,68 @@
+package com.greatlearning.TicketTracker.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.greatlearning.TicketTracker.model.Ticket;
+import com.greatlearning.TicketTracker.repository.TicketRepository;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+	@Autowired
+	TicketRepository ticketRepository;
+
+	@Override
+	public Ticket addTicket(Ticket ticket) {
+		Date newDate = new Date();
+		String formatedDate = dateFormat.format(newDate);
+		ticket.setCreated_date(formatedDate);
+		return ticketRepository.save(ticket);
+	}
+
+	@Override
+	public Ticket getTicket(long ticketId) {
+
+		return ticketRepository.findById(ticketId).get();
+	}
+
+	@Override
+	public List<Ticket> getAllTickets() {
+
+		return ticketRepository.findAll();
+	}
+
+	@Override
+	public void deleteTicket(long ticketId) {
+		ticketRepository.deleteById(ticketId);
+
+	}
+
+	@Override
+	public List<Ticket> getByKeyword(String keyword) {
+		return ticketRepository.findByKeyword(keyword);
+	}
+
+	@Override
+	public Ticket editTicket(long ticketId, Ticket ticket) {
+
+		// creating Edited date
+		Date editDate = new Date();
+		String updateDate = dateFormat.format(editDate);
+
+		Ticket ticket_db = getTicket(ticketId);
+		ticket_db.setTitle(ticket.getTitle());
+		ticket_db.setDescription(ticket.getDescription());
+		ticket_db.setContent(ticket.getContent());
+		ticket_db.setEdited_date(updateDate);
+
+		return ticketRepository.save(ticket_db);
+	}
+
+}
